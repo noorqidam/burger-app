f190586bc145beefc7fdaee85f420cbe4195ec4f
noorqidam <noorqidam@gmail.com>
2020-10-18 17:36:17 +0700

    finish checkout with setup redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 
-const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  burgerBuilderReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
 
-ReactDOM.render( app, document.getElementById( 'root' ) );
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
