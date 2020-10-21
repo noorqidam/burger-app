3832705b1dd622948a70709341c7037a12602821
noorqidam <noorqidam@gmail.com>
2020-10-21 19:43:20 +0700

    implementation to redux saga

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,6 +2,28 @@ export {
   addIngredient,
   removeIngredient,
   initIngredients,
-} from "./burgerBuilder";
-export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
-export { auth, logout, setAuthRedirectPath, authCheckState } from './auth';
+  setIngredients,
+  fetchIngredientsFailed
+} from './burgerBuilder';
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  purchaseBurgerStart,
+  purchaseBurgerFail,
+  purchaseBurgerSuccess,
+  fetchOrdersSuccess,
+  fetchOrdersStart,
+  fetchOrdersFail
+} from './order';
+export {
+  auth,
+  logout,
+  setAuthRedirectPath,
+  authCheckState,
+  logoutSucceed,
+  authStart,
+  authSuccess,
+  authFail,
+  checkAuthTimeout
+} from './auth';
